42e1d1be22e061e297944d0c77c1f29f549c0da2
dafrenchyman <dafrenchyman@gmail.com>
2025-03-31 22:44:38 -0700

    Random changes
    - Making all .sh files executable (755). Accidentally did it on all .sh files in the whole repo. I guess it wasn't on all of them?
    - Updated pegaus-plus to re-download the emulators to get newer ones (ps2 emulator was broken). ps3 had trouble.
    - Added the ability to run SCUMMVM via zip files.
      - It's not perfect, you need to add a *.scummvm file to the zip file that has the proper name of the game that you need to lookup.
      - Lookup is here: https://github.com/scummvm/scummvm-web/blob/master/data/en/games.yaml
      - You want the "id" field, but only the part after the ":".
    - Added the ability to play iso PS3 games. I haven't tested it that much. It "seems" to work, but could run slower? Unsure.

--- images/pegasus-plus/scripts/launchers/rom_launcher.sh
+++ images/pegasus-plus/scripts/launchers/rom_launcher.sh
@@ -2,6 +2,53 @@
 EMULATOR=$1
 ROM=$2
 
+ISO_MOUNT_LOCATION=/media/iso_mount
+
+function launch_ps3 {
+
+  local ROM_FILE="$1"
+
+  # Mount the folder/file if it's an iso
+  if [[ $ROM_FILE == *.iso ]]; then
+    echo "Mounting: ${ROM_FILE} to ${ISO_MOUNT_LOCATION}"
+    fuseiso "${ROM_FILE}" ${ISO_MOUNT_LOCATION}
+    ROM_FILE=${ISO_MOUNT_LOCATION}
+  fi
+
+  # Launch the game
+  /Applications/rpcs3-emu.AppImage --appimage-extract-and-run  "${ROM_FILE}"
+
+  # Unmount (if we mounted the ISO)
+  if [[ "${ROM_FILE}" == "${ISO_MOUNT_LOCATION}" ]]; then
+    echo "UnMounting: ${ISO_MOUNT_LOCATION}"
+    fusermount -u ${ISO_MOUNT_LOCATION}
+  fi
+}
+
+function launch_scummvm {
+
+  local ROM_FILE="$1"
+
+  # Mount the folder/file if it's an iso
+  if [[ $ROM_FILE == *.zip ]]; then
+    echo "Mounting: ${ROM_FILE} to ${ISO_MOUNT_LOCATION}"
+    fuse-zip -o ro "${ROM_FILE}" ${ISO_MOUNT_LOCATION}
+    ROM_FILE=${ISO_MOUNT_LOCATION}
+  fi
+
+  # Find the .scummvm filename
+  SCUMMVM_FILE=`ls ${ISO_MOUNT_LOCATION}/*.scummvm`
+
+  # Launch the game
+  retroarch -L ~/.config/retroarch/cores/scummvm_libretro.so ${ISO_MOUNT_LOCATION}/${SCUMMVM_FILE}
+
+  # Unmount (if we mounted the ISO)
+  if [[ "${ROM_FILE}" == "${ISO_MOUNT_LOCATION}" ]]; then
+    echo "UnMounting: ${ISO_MOUNT_LOCATION}"
+    fusermount -u ${ISO_MOUNT_LOCATION}
+  fi
+}
+
 # dictionary of emulator to command to run
 declare -A EMULATOR_COMMAND=( \
 ["3do"]="retroarch --fullscreen -L ~/.config/retroarch/cores/opera_libretro.so \"${ROM}\"" \
@@ -32,11 +79,12 @@ declare -A EMULATOR_COMMAND=( \
 ["psp"]="retroarch --fullscreen -L ~/.config/retroarch/cores/ppsspp_libretro.so \"${ROM}\"" \
 ["psvita"]="/Applications/vita3k-emu.AppImage --appimage-extract-and-run  \"${ROM}\"" \
 ["psx"]="retroarch --fullscreen -L ~/.config/retroarch/cores/pcsx_rearmed_libretro.so \"${ROM}\"" \
-["ps2"]="/Applications/pcsx2-emu-Qt.AppImage --appimage-extract-and-run \"${ROM}\"" \
-["ps3"]="/Applications/rpcs3-emu.AppImage --appimage-extract-and-run  \"${ROM}\"" \
+["ps2"]="/Applications/pcsx2-emu.AppImage --appimage-extract-and-run \"${ROM}\"" \
+["ps3"]="launch_ps3  \"${ROM}\"" \
 ["saturn"]="retroarch --fullscreen -L ~/.config/retroarch/cores/mednafen_saturn_libretro.so \"${ROM}\"" \
 ["sega32x"]="retroarch --fullscreen -L ~/.config/retroarch/cores/picodrive_libretro.so \"${ROM}\"" \
 ["segacd"]="retroarch --fullscreen -L ~/.config/retroarch/cores/genesis_plus_gx_libretro.so \"${ROM}\"" \
+["scummvm"]="launch_scummvm \"${ROM}\"" \
 ["snes"]="retroarch --fullscreen -L ~/.config/retroarch/cores/snes9x_libretro.so \"${ROM}\"" \
 ["snes_widescreen"]="retroarch --fullscreen -L ~/.config/retroarch/cores/bsnes_hd_beta_libretro.so \"${ROM}\"" \
 ["switch"]="/Applications/yuzu-emu.AppImage --appimage-extract-and-run -f -g \"${ROM}\"" \
--- images/pegasus-plus/setup.sh
+++ images/pegasus-plus/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Set the SHA
-export SHA="sha-0d7f3d3"
+# export SHA="sha-0d7f3d3" # old working
+# export SHA="sha-518a3b4" # current worknig
+export SHA="sha-518a3b4" # testing
 
 export PEGASUS_BASE_APP_IMAGE="ghcr.io/games-on-whales/pegasus:${SHA}"
 # export PEGASUS_BASE_APP_IMAGE="gow/pegasus:${SHA}"
@@ -9,5 +11,3 @@ export PEGASUS_BASE_APP_IMAGE="ghcr.io/games-on-whales/pegasus:${SHA}"
 echo $PEGASUS_BASE_APP_IMAGE
 
 docker build -t gow/pegasus-plus:${SHA} --build-arg="PEGASUS_BASE_APP_IMAGE=${PEGASUS_BASE_APP_IMAGE}" .
-
-
